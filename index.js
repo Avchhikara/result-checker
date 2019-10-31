c28f77505f38894b886fb54fdc0f637147c2e42d
Avnish <Avnishway@gmail.com>
2019-10-31 15:49:11 +0530

    Adding rendering functionality to the koa app

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const Koa = require("koa");
-const app = new Koa();
+const views = require("koa-views");
+const Router = require("koa-router");
 
-app.use(async ctx => {
-  ctx.body = "Hello World";
+const path = require("path");
+
+const app = new Koa();
+const router = new Router();
+
+// Adding the views middleware
+app.use(views(path.join(__dirname, "views"), { extension: "html" }));
+
+// Adding the router
+app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(async ctx => {
+//   const { req, res, render } = ctx;
+//   console.log(req, res);
+//   //   res.end("<h1>Hello World</h1>");
+//   render("hello.html");
+// });
+
+router.get("/", async ctx => {
+  await ctx.render("hello");
 });
 
 app.listen(3000);
